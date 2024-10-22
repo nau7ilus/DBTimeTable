4d18301891f514eb3426f66e1c2bda007e2ae020
nau7ilus <filip@zelinskyi.de>
2024-10-22 17:42:01 +0200

    chore(timetable comp): import clock component

--- src/app/timetable/ui/timetable/timetable.component.ts
+++ src/app/timetable/ui/timetable/timetable.component.ts
@@ -1,11 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { TimetableClockComponent } from '../../feature/timetable-clock/timetable-clock.component';
 import { TimetableHeaderComponent } from '../timetable-header/timetable-header.component';
-
 
 @Component({
   selector: 'app-timetable',
   standalone: true,
-  imports: [TimetableHeaderComponent],
+  imports: [TimetableHeaderComponent, TimetableClockComponent],
   templateUrl: './timetable.component.html',
   styleUrl: './timetable.component.scss',
   encapsulation: ViewEncapsulation.None
