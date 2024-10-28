41ecf1a7faa692966221485a5f6c8e728bc2b744
nau7ilus <filip@zelinskyi.de>
2024-10-28 09:51:24 +0100

    feat(timetable entry): initial logic

--- src/app/timetable/feature/timetable-entry/timetable-entry.component.ts
+++ src/app/timetable/feature/timetable-entry/timetable-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TrainInfo } from '../../services/train/train.codec';
 import { TimetableMarqueeComponent } from '../../ui/timetable-marquee/timetable-marquee.component';
 
 @Component({
@@ -9,5 +10,22 @@ import { TimetableMarqueeComponent } from '../../ui/timetable-marquee/timetable-
   styleUrl: './timetable-entry.component.scss'
 })
 export class TimetableEntryComponent {
+  @Input() trainEntry!: TrainInfo;
 
+  plannedTime: string = '';
+  actualTime: string = '';
+
+  ngOnInit(): void {
+    if (this.trainEntry) {
+      this.plannedTime = this.formatTime(this.trainEntry.plannedWhen);
+      this.actualTime = this.formatTime(this.trainEntry.when);
+    }
+  }
+
+  private formatTime(dateTimeString: string): string {
+    const date = new Date(dateTimeString);
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
 }
