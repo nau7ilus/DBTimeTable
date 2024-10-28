13b84259613813ab313ee3f7774d9c0e72d03eeb
nau7ilus <filip@zelinskyi.de>
2024-10-28 09:50:45 +0100

    feat(clock): add logic

--- src/app/timetable/feature/timetable-clock/timetable-clock.component.ts
+++ src/app/timetable/feature/timetable-clock/timetable-clock.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-timetable-clock',
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './timetable-clock.component.html',
   styleUrl: './timetable-clock.component.scss'
 })
-export class TimetableClockComponent {
+export class TimetableClockComponent implements OnInit, OnDestroy {
+  time = new Date();
+  private intervalId?: ReturnType<typeof setInterval>;
 
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  get hours(): string {
+    return this.pad(this.time.getHours());
+  }
+
+  get minutes(): string {
+    return this.pad(this.time.getMinutes());
+  }
+
+  private pad(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
 }
