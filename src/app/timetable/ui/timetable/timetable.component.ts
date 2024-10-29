f326e63c0539fcb133c7a96c1763d4d97f0f95e5
nau7ilus <filip@zelinskyi.de>
2024-10-29 09:38:06 +0100

    fix(tt): sort train data by the planned when date

--- src/app/timetable/ui/timetable/timetable.component.ts
+++ src/app/timetable/ui/timetable/timetable.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { startWith, switchMap } from 'rxjs/operators';
 import { TimetableClockComponent } from '../../feature/timetable-clock/timetable-clock.component';
 import { TimetableEntryComponent } from '../../feature/timetable-entry/timetable-entry.component';
 import { DepartureInfo, TrainService } from '../../services/train/train.service';
@@ -26,7 +26,7 @@ export class TimetableComponent implements OnInit, OnDestroy {
       .pipe(switchMap(() => this.trainService.getDepartures('8012666'))) // Potsdam Hbf
       .subscribe({
         next: (data) => {
-          this.trainData = data;
+          this.trainData = data.sort((a, b) => new Date(a.plannedWhen).getTime() - new Date(b.plannedWhen).getTime());
         },
         error: (error) => {
           alert('Fehler beim Datenabruf');
