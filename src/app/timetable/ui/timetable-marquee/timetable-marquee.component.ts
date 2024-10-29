a3a9802b96e7101117f078d1468a5baec498b037
nau7ilus <filip@zelinskyi.de>
2024-10-29 13:18:09 +0100

    feat: created marquee

--- src/app/timetable/ui/timetable-marquee/timetable-marquee.component.ts
+++ src/app/timetable/ui/timetable-marquee/timetable-marquee.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-timetable-marquee',
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
   templateUrl: './timetable-marquee.component.html',
   styleUrl: './timetable-marquee.component.scss'
 })
-export class TimetableMarqueeComponent {
+export class TimetableMarqueeComponent implements AfterViewInit {
+  @Input() speed = 100;
 
+  @ViewChild('marqueeContainer') marqueeContainer!: ElementRef;
+  @ViewChild('marqueeText') marqueeText!: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
+
+  get textWidth() {
+    return this.marqueeText.nativeElement.offsetWidth
+  }
+
+  get containerWidth() {
+    return this.marqueeContainer.nativeElement.offsetWidth;
+  }
+
+  ngAfterViewInit() {
+    let currentLeft = 0;
+    const speed = 1.3;
+
+    const animate = () => {
+      currentLeft = this.textWidth > this.containerWidth ? currentLeft - speed : 0;
+
+
+      this.renderer.setStyle(this.marqueeText.nativeElement, 'left', `${currentLeft}px`)
+
+      if (currentLeft < -this.textWidth) {
+        currentLeft = this.containerWidth;
+      }
+
+      requestAnimationFrame(animate)
+    }
+
+    animate()
+  }
 }
