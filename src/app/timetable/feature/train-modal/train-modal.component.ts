4658d9155aa230fca4e21f77214f308de8c0f4e9
nau7ilus <filip@zelinskyi.de>
2024-10-31 14:30:06 +0100

    feat(train modal): style and layout

--- src/app/timetable/feature/train-modal/train-modal.component.ts
+++ src/app/timetable/feature/train-modal/train-modal.component.ts
@@ -1,6 +1,6 @@
 import { NgClass } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
-import { TripInfo } from '../../services/train/train.service';
+import { AfterViewInit, Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
+import { TrainService, TripInfo } from '../../services/train/train.service';
 
 @Component({
   selector: 'app-train-modal',
@@ -14,6 +14,12 @@ export class TrainModalComponent implements AfterViewInit {
   @Input() trip!: TripInfo;
   isOpened = false;
 
+  constructor(private renderer: Renderer2) { }
+
+  getNextStopIndex() {
+    return this.trip.stopovers.findIndex(so => new Date(so.departure).getTime() > Date.now())
+  }
+
   closeModal() {
     this.dialog.nativeElement.close();
     this.dialog.nativeElement.classList.remove('opened');
@@ -24,8 +30,21 @@ export class TrainModalComponent implements AfterViewInit {
     this.dialog.nativeElement.classList.add('opened');
   }
 
+  scrollToLastStation(): void {
+    const targetElement = this.renderer.selectRootElement(`#station-${this.getNextStopIndex()}`, true);
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+
+  formatTime(dateTimeString: string): string {
+    return TrainService.formatTime(dateTimeString);
+  }
+
   ngAfterViewInit() {
     if (this.isOpened) this.openModal()
+
+    setTimeout(() => {
+      this.scrollToLastStation()
+    }, 600)
 
     this.dialog.nativeElement.addEventListener('click', (event: MouseEvent) => {
       const target = event.target as Element;
